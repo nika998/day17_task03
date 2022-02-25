3e3005ccf46bed29b931739d859694bbe57e7a1a
Tomislav Tadic <tomislav.tadic.89@gmail.com>
2022-02-25 11:34:46 +0100

    Added UserInterface features to implement

--- day17_task03/src/user/UserInterface.java
+++ day17_task03/src/user/UserInterface.java
@@ -3,5 +3,12 @@ package user;
 public interface UserInterface {
 	
 	void getPassword();
+	
+	String getUserDetails();
+	
+	UserInterface update(Integer id, UserInterface updatedUser);
 
+	UserInterface createAdmin(Integer id, String lastName, String password);
+
+	UserInterface createEditor(Integer id, String lastName, String password);
 }
